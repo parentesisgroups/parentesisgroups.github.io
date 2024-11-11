d538c9735d5ecd9762ec234aaa733075539834fa
Carlosvleon <vallejos.leon.carlos@gmail.com>
2024-11-11 12:07:58 -0300

    Update footer content fetch path in index.js to correct relative URL

--- js/html/index/index.js
+++ js/html/index/index.js
@@ -1,5 +1,5 @@
 // Usamos JavaScript para cargar el contenido del footer
-fetch('html/footer/footer.html')
+fetch('../html/footer/footer.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer-container').innerHTML = data;
